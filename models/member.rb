594ef3779be2e66ed781a9a52d1209ea7d3b0c5f
Robert Tam <rsh.tam@gmail.com>
2019-06-07 21:27:36 +0100

    Implemented delete method for member class

--- models/member.rb
+++ models/member.rb
@@ -38,6 +38,12 @@ class Member
     SqlRunner.run(sql, values)
   end
 
+  def delete
+    sql = "DELETE FROM members WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
   def self.all
     sql = "SELECT * FROM members"
     members_data = SqlRunner.run(sql)
