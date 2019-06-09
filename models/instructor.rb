63dc73b790b75f58784d3119dc7f0c340e02a6d6
Robert Tam <rsh.tam@gmail.com>
2019-06-09 21:19:58 +0100

    Instructor update method added

--- models/instructor.rb
+++ models/instructor.rb
@@ -21,4 +21,14 @@ class Instructor
     @id = SqlRunner.run(sql, values)[0]["id"]
   end
 
+  def update
+    sql = "UPDATE instructors SET (
+      first_name, last_name
+    ) = (
+      $1, $2
+    ) WHERE id = $3"
+    values = [@first_name, @last_name, @id]
+    SqlRunner.run(sql, values)
+  end
+
 end
