5237f4ceea1fa37026f982379b01642f5d6153ce
Robert Tam <rsh.tam@gmail.com>
2019-06-09 21:21:28 +0100

    Instructor delete methods added

--- models/instructor.rb
+++ models/instructor.rb
@@ -31,4 +31,15 @@ class Instructor
     SqlRunner.run(sql, values)
   end
 
+  def delete
+    sql = "DELETE FROM instructors WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all
+    sql = "DELETE FROM instructors"
+    SqlRunner.run(sql)
+  end
+
 end
